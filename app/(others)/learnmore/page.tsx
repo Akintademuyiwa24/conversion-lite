cdbdeb3b4b7c715c2fb248a29d881429ae064086
Akintade Olumuyiwa <akintadeolumuyiwa@CGs-MacBook-Pro.local>
2025-08-20 13:30:17 +0100

    modified the marquee on the leanrmore page

--- app/(others)/learnmore/page.tsx
+++ app/(others)/learnmore/page.tsx
@@ -26,7 +26,7 @@ export const AnnouncementTicker = () => {
       >
         <span className="mx-4">Click here for 40+ new updatess</span>
         <span className="mx-4">Best partners by your side</span>
-        <span className="mx-4">Sell everywhere from one dashboard</span>
+        <span className="mx-4">Sell everywhere from just one dashboard</span>
         <span className="mx-4">Free plan available – no credit card needed</span>
       </Marquee>
     </div>
